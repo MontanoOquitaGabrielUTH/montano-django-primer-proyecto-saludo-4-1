aad282d6d2376c06ae155bd3b37231c58ee81a43
MontanoOquitaGabrielUTH <a24311016@uthermosillo.edu.mx>
2025-09-08 09:15:49 -0700

    Primer commit: proyecto Django inicial

---
+++ mensaje/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+from django.http import HttpResponse 
+
+def saludo(request): 
+    return HttpResponse("Hola Soy Montaño desde Django, listo para PythonAnywhere!") 
